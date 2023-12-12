078bbff05f7eb13b97be305c4e58e8c629998631
Philip Chng <pczn0327@gmail.com>
2023-12-12 23:04:20 +0800

    [Test] Add for SignOutService

---
+++ test/src/services/sign_out/sign_out_service_test.dart
@@ -0,0 +1,91 @@
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:auth_repo/auth_repo.dart';
+import 'sign_out_service_test.mocks.dart';
+
+@GenerateNiceMocks([
+  MockSpec<FirebaseAuth>(),
+  MockSpec<GoogleSignIn>(),
+])
+void main() {
+  final firebaseAuth = MockFirebaseAuth();
+  final googleSignIn = MockGoogleSignIn();
+  final signOutService = SignOutService(
+    firebaseAuth: firebaseAuth,
+    googleSignIn: googleSignIn,
+  );
+  final signOutServiceWithoutGoogle = SignOutService(
+    firebaseAuth: firebaseAuth,
+  );
+
+  group('SignOutService', () {
+    group('firebase', () {
+      test('successfully', () async {
+        when(firebaseAuth.signOut()).thenAnswer((_) => Future.value());
+
+        try {
+          await signOutService.firebase();
+        } catch (e) {
+          fail('Did not pass as expected');
+        }
+
+        verify(firebaseAuth.signOut()).called(1);
+      });
+
+      test('throws SignOutException', () async {
+        when(firebaseAuth.signOut()).thenThrow(Exception());
+
+        try {
+          await signOutService.firebase();
+          fail('Did not throw as expected');
+        } catch (e) {
+          expect(e, isA<SignOutException>());
+        }
+
+        verify(firebaseAuth.signOut()).called(1);
+      });
+    });
+
+    group('google', () {
+      test('successfully', () async {
+        when(googleSignIn.signOut()).thenAnswer((_) => Future.value());
+
+        try {
+          await signOutService.google();
+        } catch (e) {
+          fail('Did not pass as expected');
+        }
+
+        verify(googleSignIn.signOut()).called(1);
+      });
+
+      test('throws SignOutException', () async {
+        when(googleSignIn.signOut()).thenThrow(Exception());
+
+        try {
+          await signOutService.google();
+          fail('Did not throw as expected');
+        } catch (e) {
+          expect(e, isA<SignOutException>());
+        }
+
+        verify(googleSignIn.signOut()).called(1);
+      });
+
+      test('throws MissingDependencyException', () async {
+        try {
+          await signOutServiceWithoutGoogle.google();
+          fail('Did not throw as expected');
+        } catch (e) {
+          expect(e, isA<MissingDependencyException>());
+        }
+      });
+    });
+  });
+}
